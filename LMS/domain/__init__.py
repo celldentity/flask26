049049188ebf9fe6527a979c12bb10cb9858bf31
320-05\ksb2116 <hc231106@gmail.com>
2026-02-19 12:16:47 +0900

    260219 테스트 코딩파일

--- LMS/domain/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Member import Member
-from .Score import Score
-from .Board import Board
-
-__all__ = ['Member', 'Score', 'Board']
--- LMS/service/__init__.py
+++
@@ -1,6 +0,0 @@
-from .MemberService import MemberService
-from .ScoreService import ScoreService
-from .BoardService import BoardService
-from .PostService import PostService
-
-__all__ = ['MemberService', 'ScoreService', 'BoardService', 'PostService']
